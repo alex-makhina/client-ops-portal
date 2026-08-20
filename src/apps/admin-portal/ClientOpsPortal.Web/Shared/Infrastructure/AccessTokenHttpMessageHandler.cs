76630d6785fa73acc493d97816352f25665bf2eb
Makhina A.I <makhina@irc.beltelecom.by>
2026-08-20 16:15:56 +0300

    fix AccessTokenHttpMessageHandler

--- src/apps/admin-portal/ClientOpsPortal.Web/Shared/Infrastructure/AccessTokenHttpMessageHandler.cs
+++ src/apps/admin-portal/ClientOpsPortal.Web/Shared/Infrastructure/AccessTokenHttpMessageHandler.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using System.Net;
 using System.Net.Http.Headers;
 
 namespace ClientOpsPortal.Web.Shared.Infrastructure
@@ -6,15 +8,23 @@ namespace ClientOpsPortal.Web.Shared.Infrastructure
     public class AccessTokenHttpMessageHandler : DelegatingHandler
     {
         private readonly IAccessTokenProvider _tokenProvider;
+        private readonly NavigationManager _navigationManager;
 
-        public AccessTokenHttpMessageHandler(IAccessTokenProvider tokenProvider)
+        public AccessTokenHttpMessageHandler(IAccessTokenProvider tokenProvider, NavigationManager navigationManager)
         {
             _tokenProvider = tokenProvider;
+            _navigationManager = navigationManager;
         }
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             var result = await _tokenProvider.RequestAccessToken();
+
+            if (result.Status == AccessTokenResultStatus.RequiresRedirect)
+            {
+                _navigationManager.NavigateToLogin(result.InteractiveRequestUrl!, result.InteractionOptions!);
+                return new HttpResponseMessage(HttpStatusCode.Unauthorized);
+            }
 
             if (result.TryGetToken(out var token))
             {
